3fac45cccd5dbed7bc1875180a96d7d06d6049ab
p0nch000 <A01198400@tec.mx>
2024-06-28 12:16:30 -0600

    testing

--- config/db.js
+++ config/db.js
@@ -4,12 +4,15 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-        host: process.env.DB_HOST,
-        dialect: "postgres",
-        port: process.env.DB_PORT
-    }
-);
+    process.env.DB_NAME, 
+    process.env.DB_USER, 
+    process.env.DB_PASSWORD, {  
+      host: process.env.DB_HOST,
+      dialect: 'postgres',
+      port: process.env.DB_PORT,
+      //dialectOptions: {
+        //ssl: {
+          //require: true,
+          //rejectUnauthorized: false 
+        //}
+      });
---
+++ queries/test.sql
@@ -0,0 +1,3 @@
+UPDATE links
+SET affiliate_link = 'tu_enlace_de_afiliado_aqui'
+WHERE affiliate_link IS NULL;
